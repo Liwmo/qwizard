01bd32d6db6642193a0987669403ae6313bff2e2
intern <intern@asynchrony.com>
2015-06-23 13:45:36 -0500

    Commiting work done at this workstation.

--- public/taker/directives/question.js
+++ public/taker/directives/question.js
@@ -9,9 +9,12 @@ app.directive("question", function(){
 		},
 		template: '<div ng-include="getType()"></div>',
 		link: function(scope, elem, attrs){
-			scope.tf = function(c){
-				scope.selected = c;
+			scope.tf = function(answer){
+				scope.selected = answer;
 			};
+
+			scope.mc = scope.tf;
+
 			scope.getType = function(){
 				return '/taker/directives/templates/' + scope.type + '.html';
 			}
