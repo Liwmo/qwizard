b8885ea4062bb09e9d25444384a14188c1dd7691
username <station3@qwizard.com>
2015-06-24 14:00:37 -0500

    Karma testing fixed - JH & MK

---
+++ public/taker/directives/question-test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+describe('Question Directive', function() {
+    var $scope, element;
+
+    beforeEach(module('app'));
+    beforeEach(module('my.templates'));
+    beforeEach(inject(function($compile, $rootScope) {
+        $scope = $rootScope.$new();
+
+        element = '<question text="text" name="name" type="type" selected="selected"></question>';
+
+        $scope.text = "What is yo name?";
+        $scope.name = "Get to know you";
+        $scope.type = "tf";
+        $scope.selected = null;
+        element = $compile(element)($scope);
+
+        $scope.$digest();
+
+    }));
+
+    it('scope.name is passed in properly', inject(function($controller) {
+        expect(element.children()[0].innerHTML).toBe($scope.name);
+    }));
+
+    it('scope.text is passed in properly', inject(function($controller) {
+        expect(element.children()[2].innerHTML).toBe($scope.text);
+    }));
+
+    it('scope.type is templated correctly when tf', inject(function($controller) {
+        expect(element.children()[3].children.length).toBe(2);
+        expect(element.children()[3].children[0].innerHTML).toBe("True");
+        expect(element.children()[3].children[1].innerHTML).toBe("False");
+    }));
+});
