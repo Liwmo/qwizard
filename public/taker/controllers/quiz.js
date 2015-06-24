6ce0886a8deec4a5968c03590bc208e072145a7a
intern <intern@intern.org>
2015-06-24 14:54:42 -0500

    multiple choice implemented MK WL

--- public/taker/controllers/quiz.js
+++ public/taker/controllers/quiz.js
@@ -2,7 +2,7 @@ app.controller('quiz', function($scope) {
     $scope.name = "WWT Quiz";
     
     $scope.questions = [
-    	{name: "Apple Pie", text: "Question 1 is this one?!", answers: [], type: "tf", selected: null},
+    	{name: "Apple Pie", text: "Question 1 is this one?!", answers: ["pizza", "sushi", "taco"], type: "mc", selected: null},
     	{name: "Banana Bread", text: "Question 2", answers: [], type: "tf", selected: null},
    		{name: "Creamsicle", text: "Question 3", answers: [], type: "tf", selected: null}
    	];
