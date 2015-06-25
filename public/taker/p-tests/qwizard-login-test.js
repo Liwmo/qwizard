e67b24331c103b014b30900b819d41bae90c29c7
intern <intern@asynchrony.com>
2015-06-25 15:23:42 -0500

    added comment to login page UAT test

--- public/taker/p-tests/qwizard-login-test.js
+++ public/taker/p-tests/qwizard-login-test.js
@@ -1,6 +1,10 @@
 describe('Qwizard Homepage', function() {
   it('should have a submit button', function() {
+  	
+  	//Use this for any pages that don't have angular
   	browser.ignoreSynchronization = true;
+
+  	
     browser.get('http://localhost:3000/');
     console.log(element(by.css('[type="submit"]')));
     element(by.css('[type="submit"]')).click();
