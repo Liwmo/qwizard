6d65cabe7c67ec40bcf874408acbb8a269b4b0da
intern <intern@intern.org>
2015-06-29 13:02:40 -0500

    Dashboard leaders limited to 10 surrounding current user - NS WL

--- public/taker/controllers/dashboard.js
+++ public/taker/controllers/dashboard.js
@@ -3,14 +3,29 @@ app.controller('dashboard', ['$scope', 'notificationFactory', 'leaderboardFactor
 
     $scope.notifications = notificationFactory.getNotifications();
 
-    leaderboardFactory.getLeaders(function(data) {
-      $scope.leaders = data.users;
-    });
-
     $scope.currentUser = {
       userid: 507,
     	name: 'me.me',
     	score: 600,
     	rank: 85
     };
+
+    leaderboardFactory.getLeaders(function(data) {
+      var userIndex = -1;
+      for(var i = 0; i < data.users.length; i++) {
+        if($scope.currentUser.userid === data.users[i].userid) {
+          userIndex = i;
+        }
+      }
+      var min = Math.max(userIndex - 5, 0);
+      var max = Math.min(min+10, data.users.length - 1);
+      min = max - 10;
+      var users = [];
+      for(var i = min; i < max; i++) {
+          users.push(data.users[i]);
+      }
+      $scope.leaders = users;
+      $scope.start = min + 1;
+    });
+
 }]);
