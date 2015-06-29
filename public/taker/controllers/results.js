d3d258398b171a58d235964177df98a7199f2c84
intern <intern@intern.org>
2015-06-29 17:41:52 -0500

    Added view for results points display - DK MK

--- public/taker/controllers/results.js
+++ public/taker/controllers/results.js
@@ -1,12 +1,20 @@
 app.controller('results', ["$scope", "quizFactory", function($scope, quizFactory) {
     $scope.name = "";
     $scope.currentQuestion = 0;
-
-
+    $scope.points = 10;
+    $scope.max_points = 30;
+    
 
     quizFactory.getQuiz("dummy_id", function(data){
         $scope.name = data.title;
         $scope.questions = data.questions;
+
+        $scope.ratio = $scope.points / $scope.max_points;
+    	$scope.title = ($scope.ratio > .5 ? "Congrats!" : "Uh oh!");
+
+        $scope.message = $scope.points + " points earned on " + $scope.name;
+    	$scope.message += ($scope.ratio > .5 ? "!" : ".");
+    	$scope.message += ($scope.ratio > .5 ? "" : "Better study more next time!");
     });
 
 
