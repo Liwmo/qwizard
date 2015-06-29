8a36b519d8eeb7ea57078d69501932d054c2d3a1
intern <intern@intern.org>
2015-06-29 15:25:38 -0500

    Fixed bug when incorrect parameters are passed to the Quiz/Post endpoint - DK JH

--- routes/api/quiz.js
+++ routes/api/quiz.js
@@ -10,11 +10,12 @@ router.route('/')
 
 router.route('/:id')
 	.get(function(req, res){
-		var id = (req.params.id == "dummy_id") ? 1 : parseInt(req.params.id);
+		var id = (req.params.id == "dummy_id") ? 1 : req.params.id;
 		db.getConnection(function(err, connection) {
+			console.log('Select quiz from quizzes where id=' + id);
 			var query = connection.query('Select quiz from quizzes where id=' + id, function(err, message){
 				connection.release();
-				if(!err) {
+				if(!err && message.length) {
 					res.send(message[0].quiz);
 				}
 				else {
@@ -26,18 +27,22 @@ router.route('/:id')
 		//res.sendFile(path.join(__dirname, "../../mockData/mockData.json"));
 	})
 	.post(function(req, res){
-		var id = (req.params.id == "dummy_id") ? 1 : parseInt(req.params.id);
+		var id = (req.params.id == "dummy_id") ? 1 : req.params.id;
 		db.getConnection(function(err, connection) {
+			console.log('Select answers from quizzes where id=' + id);
 			var query = connection.query('Select answers from quizzes where id=' + id, function(err, message){
 				connection.release();
-				if(!err) {
+				if(!err && message.length) {
 					var answers = JSON.parse(message[0].answers);
-					if(req.params.id == "dummy_id"){
+					if(id == 1){
 						answers = answers.slice(3, 10);
 					}
 
 					var selected = req.body;
-					console.log(typeof selected, selected);
+					if(selected.length !== answers.length){
+						res.send("error: answer length mismatch");
+						return;
+					}
 					var score = 0;
 
 					var pointValue = {
