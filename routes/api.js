5a8b05fdaeff283d20126de71c2874a18e274958
intern <intern@asynchrony.com>
2015-06-30 15:41:58 -0500

    added cookie verification for api endpoints - JH & MF

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,25 @@ var router = express.Router();
 var quiz = require('./api/quiz');
 var leaderboard = require('./api/leaderboard');
 var userScore = require('./api/userScore');
+var db = require('../database/db.js');
+
+router.all('/*', function(req, res, next){
+	console.log('route caught, verifying');
+	db.getConnection(function(err, connection){
+		if(!err){
+			var query = connection.query("select * from tokens where cookie=?", req.cookies.login, function(err, message){
+				if(!err && message.length){
+					next();
+				}else{
+					res.send('error: improper credentials');
+				}
+				connection.release();
+			});
+		}else{
+			res.send('error: no db connection');
+		}
+	});
+});
 
 router.use('/quiz', quiz);
 router.use('/leaderboard', leaderboard);
--- routes/taker.js
+++ routes/taker.js
@@ -3,46 +3,6 @@ var router = express.Router();
 var path = require('path');
 var ldap = require('ldapjs');
 var db = require('../database/db');
-// var passport = require('passport');
-// var LdapStrategy = require('passport-ldapauth');
-// var LdapSimpleStrategy = require('passport-ldapbind').Strategy;
-	
-// var OPTS = {
-// 	server:{
-// 		url: 'ldap://dc0.authdev.lan:389',
-// 		// bindDn: 'cn=administrator,cn=Users,dc=authdev,dc=lan', 		// :D 
-// 		// bindCredentials: 'P@$$W0rd',
-// 		searchBase: 'ou=passport-ldapauth',
-// 		searchFilter: '(uid={{username}})'
-// 	}
-// };
-
-// passport.use(new LdapStrategy(OPTS));
-
-//passport.use(new LdapSimpleStrategy({}));
-
-//router.post('/', passport.authenticate('ldapauth', {session: false}));
-
-// router.post('/', function(req, res, next) {
-//   passport.authenticate('ldapauth', function(err, user, info) {
-//   	console.log(err.message);
-//   	if(!err)
-//   		res.redirect('/taker');
-//   	else
-//   		res.send('error');
-//   })(req, res, next);
-// });
-
-// router.post('/', function(req, res, next) {
-//     var username = req.body.username;
-//     var password = req.body.password;
-//     console.log("This is " + username + " of " + password);
-
-//     res.redirect('/taker');
-// });
-
-
-
 
 function guid() {
   function s4() {
@@ -69,23 +29,17 @@ router.post('/', function(req, res, next) {
 	});
 
     client.bind('CN=' + username + ',OU=Employees,OU=UsersAccounts,OU=StLouis,DC=schafer,DC=lan', password, function(err, ldapRes) {
-    	if (err) {
+    	if(err){
     		console.log(err.message);
     		res.redirect('/');
-    	}
-    	else {
-    		console.log("hey you logged in successfully!");
+    	}else{
     		var cookie = guid();
     		db.getConnection(function(err, connection){
-    			console.log("INSERT INTO tokens values('" + cookie + "','" + username + "')");
-    			console.log("Adding your token to the DB");
-    			var query = connection.query("INSERT INTO tokens values('" + cookie + "','" + username + "')", function(err, message){
-    				if (err) {
-    					console.log(err.message);
+    			var query = connection.query("INSERT INTO tokens SET ?", {cookie: cookie, user: username}, function(err, message){
+    				if(err){
     					res.redirect('/');
-    				}
-    				else {
-    					res.cookie('login', cookie, {maxAge: 900000});
+    				}else{
+    					res.cookie('login', cookie, {maxAge: 365 * 24 * 60 * 60 * 1000});
     					res.redirect('/taker');
     				}
     			});
