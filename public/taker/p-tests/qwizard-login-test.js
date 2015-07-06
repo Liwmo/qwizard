1c90547674a3076a5644ffef25d54a6bfa633a5c
intern <intern@intern.org>
2015-07-06 09:59:29 -0500

    Began writing UATs for Login view

--- public/taker/p-tests/qwizard-login-test.js
+++ public/taker/p-tests/qwizard-login-test.js
@@ -1,11 +1,35 @@
 describe('Qwizard Homepage', function() {
-  it('should have a submit button', function() {
+
+	it ("Should not allow browsing directly to any taker webpage", function() {
+
+		var url = 'http://localhost:3000/taker/#/';
+		browser.get(url);
+		browser.sleep(500);
+		expect(browser.getCurrentUrl()).toNotBe(url);
+	});
+
+	it ("Should redirect to login when attempting to browse without logging in", function() {
+		browser.get("http://localhost:3000/taker/#/quiz");
+		browser.sleep(500);
+		expect(browser.getCurrentUrl()).toBe("http://localhost:3000/");
+	});
+
+	it ("Should not allow invalid credentials", function() {
+		browser.get('http://localhost:3000');
+		browser.sleep(500);
+		element(by.css('[type="text"]')).sendKeys('blah_not_a_real_user');
+		element(by.css('[type="password"]')).sendKeys('blah_not_a_real_password');
+		element(by.css('[type="submit"]')).click();
+		browser.sleep(500);
+		expect(browser.getCurrentUrl()).toNotBe('http://localhost:3000/taker/#/');
+	});
+  // it('should have a submit button', function() {
   	
-  	//Use this for any pages that don't have angular
-  	browser.ignoreSynchronization = true;
+  // 	//Use this for any pages that don't have angular
+  // 	browser.ignoreSynchronization = true;
 
   	
-    browser.get('http://localhost:3000/');
-    element(by.css('[type="submit"]')).click();
-  });
+  //   browser.get('http://localhost:3000/');
+  //   element(by.css('[type="submit"]')).click();
+  // });
 });
