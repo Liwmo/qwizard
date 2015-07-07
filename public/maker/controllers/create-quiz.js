9b2d6c09c2e4d29e2d4e9b5c2bce643cbe641477
intern <intern@asynchrony.com>
2015-07-07 16:02:17 -0500

    Notifies when invalid quizName - DK NS

--- public/maker/controllers/create-quiz.js
+++ public/maker/controllers/create-quiz.js
@@ -1,31 +1,41 @@
 app.controller('create-quiz', ['$scope', '$location', function($scope, $location) {
 
-	$scope.quizName = "Balogna";
 
     $scope.popupToggle = function() {
         document.querySelector('.popup').classList.toggle('visible');
-    }
+    };
 
     $scope.toDashboard = function() {
     	$location.path('/');
-    }
+    };
 
     $scope.verifyName = function() {
     	var pattern = new RegExp("^[A-Za-z0-9_]*[A-Za-z0-9][A-Za-z0-9 _'-]*$");
     	console.log(pattern.test($scope.quizName));
-    }
-
-    $scope.sendName = function() {
-    	console.log("Sending Name to server to validate Special Snowflakeness");
-    }
+    	return pattern.test($scope.quizName);
+    };
 
     $scope.saveDraft = function() {
-    	$scope.popupText = "Your draft is saved would you like to continue?"
-    	$scope.leftButton = "No, return to dashboard";
-    	$scope.rightButton = "Yes, I'm still workin'"
-    	console.log("Saving current draft");
-    	$scope.popupToggle();
-    }
+    	if (!$scope.verifyName()) {
+    		console.log("I can't save this name");
+    	}
+    	else {
+    		$scope.popupText = "Your draft is saved would you like to continue?"
+	    	$scope.leftButton = "No, return to dashboard";
+	    	$scope.rightButton = "Yes, I'm still workin'"
+	    	console.log("Saving current draft");
+	    	$scope.popupToggle();
+    	}
+    };
+
+    $scope.publishQuiz = function() {
+    	if (!$scope.verifyName()) {
+    		console.log("I can't publish with this quiz name");
+    	}
+    	else {
+    		console.log("Publishing the quiz to server");
+    	}
+    };
 
     $scope.cancelConfirm = function() {
     	$scope.popupText = "Unsaved changes will be thrown on the ground. Really cancel?"    	
@@ -33,6 +43,6 @@ app.controller('create-quiz', ['$scope', '$location', function($scope, $location
     	$scope.rightButton = "No, I'm still workin'";
     	console.log("Confirm that cancellation");
     	$scope.popupToggle();
-    }
+    };
 
 }]);
