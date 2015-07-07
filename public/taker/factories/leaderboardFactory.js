e079ec11605ab0d201c8b4574ebfe51b712006fc
intern <intern@intern.org>
2015-07-07 15:44:27 -0500

    Monthly and Weekly leaderboards implemented -MK WL JH

--- public/taker/factories/leaderboardFactory.js
+++ public/taker/factories/leaderboardFactory.js
@@ -11,5 +11,31 @@ app.factory("leaderboardFactory", ["$http", function($http){
 		});
 	};
 
+	self.getLeadersSinceDate = function(date, callback){
+		var dateStr = date.toISOString().substr(0,10);
+		$http.get("/api/leaderboard/since/" + dateStr).success(function(data){
+			if (!data.error && data.users) {
+				data.users.sort(function(a,b){return b.score - a.score});
+			}
+			callback(data);
+		});
+	};
+
+	self.getMonthly = function(callback){
+		var date = new Date();
+		var month = date.getMonth() + 1;
+		var year = date.getFullYear();
+		var dateStr =  year + '-' + month;
+		date = new Date(dateStr);
+		self.getLeadersSinceDate(date, callback);
+	}
+
+	self.getWeekly = function(callback){
+		var date = new Date();
+		date.setDate(date.getDate() - 7);
+		console.log(date);
+		self.getLeadersSinceDate(date, callback);
+	}
+
 	return self;
 }]);
--- routes/api/leaderboard.js
+++ routes/api/leaderboard.js
@@ -20,6 +20,29 @@ router.route('/')
 		//res.sendFile(path.join(__dirname, "../../mockData/mockScores.json"));
 	});
 
+router.route('/since/:date')
+	.get(function(req, res){
+		var date = new Date(req.params.date);
+
+		if(date.toString == "Invalid Date"){
+			res.send({error: 'invalid date'});
+			return;
+		}
+
+		var query =		'select name, userid, sum(points) as score ';
+			query +=	'from users as u, results as r, quizzes as q ';
+			query += 	'where r.userid=u.id and r.quizid=q.id and q.results>=? ';
+			query += 	'group by userid';
+		console.log(query);
+		db.query(query, date, function(err, message) {
+			if(err) {
+				res.send({error: 'cannot access leaderboard data'});
+			}else{
+				res.send({users: message});
+			}
+		})
+	});
+
 router.route('/:userid')
 	.get(function(req, res){
 		res.sendFile(path.join(__dirname, "../../mockData/mockScores.json"));
