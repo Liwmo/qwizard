c1a31967473df45dde7bbd47b2e6550431c6616a
intern <intern@asynchrony.com>
2015-07-07 15:31:24 -0500

    Dynamic Text and redirect when clicking leftButton - NS DK

--- public/maker/controllers/create-quiz.js
+++ public/maker/controllers/create-quiz.js
@@ -1,9 +1,13 @@
-app.controller('create-quiz', ['$scope', function($scope) {
+app.controller('create-quiz', ['$scope', '$location', function($scope, $location) {
 
 	$scope.quizName = "Balogna";
 
-    $scope.popupCancel = function() {
+    $scope.popupToggle = function() {
         document.querySelector('.popup').classList.toggle('visible');
+    }
+
+    $scope.toDashboard = function() {
+    	$location.path('/');
     }
 
     $scope.verifyName = function() {
@@ -13,6 +17,22 @@ app.controller('create-quiz', ['$scope', function($scope) {
 
     $scope.sendName = function() {
     	console.log("Sending Name to server to validate Special Snowflakeness");
+    }
+
+    $scope.saveDraft = function() {
+    	$scope.popupText = "Your draft is saved would you like to continue?"
+    	$scope.leftButton = "No, return to dashboard";
+    	$scope.rightButton = "Yes, I'm still workin'"
+    	console.log("Saving current draft");
+    	$scope.popupToggle();
+    }
+
+    $scope.cancelConfirm = function() {
+    	$scope.popupText = "Unsaved changes will be thrown on the ground. Really cancel?"    	
+    	$scope.leftButton = "Yes, just drop it";
+    	$scope.rightButton = "No, I'm still workin'";
+    	console.log("Confirm that cancellation");
+    	$scope.popupToggle();
     }
 
 }]);
