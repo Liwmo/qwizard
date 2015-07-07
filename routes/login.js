52764fed4bfa2fa6246ca4c5e094c1512ac3f444
username <station3@qwizard.com>
2015-07-07 16:05:37 -0500

    refactored login to post to root and changed authenticateCookie differentiate between API and non-API calls - MF JH

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var ldap = require('ldapjs');
 var db = require('../database/db');
+var convert = require('./userConversion');
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
 
 router.get('/logout', function(req, res) {
     console.log('logging out');
@@ -26,6 +39,72 @@ router.get('/', function(req, res, next) {
 
         res.render('index', { showBadCredentialsError: showBadCredentialsError });
     }
+});
+
+router.post('/', function(req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    if(!username || !password){
+        var badCredentials = encodeURIComponent('true');
+        res.redirect('/?badCredentials=' + badCredentials);
+        return;
+    }
+
+    if(req.cookies.login){
+        res.redirect('/taker');
+        return;
+    }
+
+    //console.log("This is " + username + " of " + password);
+    var tlsOptions = { 'rejectUnauthorized': false }
+    var client = ldap.createClient({
+        url: 'ldaps://dc3-stl.schafer.lan:636',
+        tlsOptions: tlsOptions
+    });
+
+    if (username == 'proj-1189-bind') {
+        console.log("ALERT: Logging in with the Resource Account");
+        var bindPath = 'CN='+ username +',OU=ServiceAccounts,OU=UsersAccounts,OU=StLouis,DC=schafer,DC=lan';
+    }
+    else {
+        var bindPath = 'CN=' + username + ',OU=Employees,OU=UsersAccounts,OU=StLouis,DC=schafer,DC=lan';
+    }
+
+    client.bind(bindPath, password, function(err, ldapRes) {
+        if(err){
+            console.log(err.message);
+            var badCredentials = encodeURIComponent('true');
+            res.redirect('/?badCredentials=' + badCredentials);
+        }else{
+            var cookie = guid();
+            convert.nameToId(username, function(result){
+                if(!result){
+                    db.query("INSERT INTO users (name) VALUES(?)", username, function(err, message){
+                        convert.nameToId(username, function(result){
+                            db.query("INSERT INTO tokens SET ?", {cookie: cookie, userid: result}, function(err, message){
+                                if(err){
+                                    res.redirect('/');
+                                }else{
+                                    res.cookie('login', cookie, {maxAge: 365 * 24 * 60 * 60 * 1000});
+                                    res.redirect('/taker');
+                                }
+                            });
+                        });
+                    });
+                }else{
+                    db.query("INSERT INTO tokens SET ?", {cookie: cookie, userid: result}, function(err, message){
+                        if(err){
+                            res.redirect('/');
+                        }else{
+                            res.cookie('login', cookie, {maxAge: 365 * 24 * 60 * 60 * 1000});
+                            res.redirect('/taker');
+                        }
+                    });
+                }
+            });
+            client.unbind(function(err) {if (err) console.log("You cannot leave the LDAP!!!!"); else console.log("Unbinding from the LDAP!")});
+        }
+    });
 });
 
 module.exports = router;
