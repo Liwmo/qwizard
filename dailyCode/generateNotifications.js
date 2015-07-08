c66b966ab10c58b5c19bd3af31d0b66e62840a08
username <station3@qwizard.com>
2015-07-08 16:54:42 -0500

    142 - added removal of 'old' available notifications - JH

--- dailyCode/generateNotifications.js
+++ dailyCode/generateNotifications.js
@@ -4,7 +4,28 @@ var today = (new Date()).toISOString().substr(0,10);
 
 //   insert into notifications (quizId,userId,typeId)  select q.id, u.id, 1  from quizzes q join users u where q.publish = '2015-07-08'
 
-var addAvailableNotifications = function(callback, onError) {
+var tasks = new (function(){
+	var self = this;
+	var list = [];
+
+	var next = function(){
+		if(list.length){
+			var task = list.shift();
+			task(next);
+		}
+	};
+
+	self.add = function(task){
+		list.push(task);
+		return self;
+	};
+
+	self.start = function(){
+		next();
+	};
+})();
+
+var addAvailableNotifications = function(next) {
 	var query =  'insert into notifications (quizId, userId, typeID) ';
 		query += 'select q.id, u.id, 1 from quizzes q join users u where q.publish=?';
 
@@ -14,11 +35,26 @@ var addAvailableNotifications = function(callback, onError) {
 		}else{
 			console.log("success");
 		}
-		callback();
+		next();
 	});
 };
 
-var addResultsNotifications = function(callback, onError) {
+var removeAvailableNotifications = function(next){
+	var query =  'delete from notifications ';
+		query += 'where typeId=1 and quizId in ';
+		query += '(select quizId from quizzes where results=?)';
+
+	db.query(query, today, function(err, message){
+		if(err){
+			console.log(err);
+		}else{
+			console.log("success");
+		}
+		next();
+	});
+};
+
+var addResultsNotifications = function(next) {
 	var query =  'insert into notifications (quizId, userId, typeID) ';
 		query += 'select q.id, u.id, 2 from quizzes q join users u where q.results=?';
 
@@ -28,7 +64,7 @@ var addResultsNotifications = function(callback, onError) {
 		}else{
 			console.log("success");
 		}
-		callback();
+		next();
 	});
 };
 
@@ -36,12 +72,9 @@ var accomplishedExit = function() {
 	process.exit();
 };
 
-
-var errorExit = function() {
-	console.log("did not succeed");
-	process.exit();
-};
-
-addAvailableNotifications(function(){
-	addResultsNotifications(accomplishedExit, errorExit);
-}, errorExit);
+tasks
+	.add(addAvailableNotifications)
+	.add(removeAvailableNotifications)
+	.add(addResultsNotifications)
+	.add(accomplishedExit)
+	.start();
