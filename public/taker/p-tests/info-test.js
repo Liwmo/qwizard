5f40736ff6469dff95d2596416264b56082e0825
intern <intern@asynchrony.com>
2015-07-09 17:42:27 -0500

    Beginning box flexbox for info paragraphs - DK MF

--- public/taker/p-tests/info-test.js
+++ public/taker/p-tests/info-test.js
@@ -24,4 +24,17 @@ describe('info page', function() {
     element(by.css('#drawer a[href="#/info"]')).click();
     expect(browser.getCurrentUrl()).toBe('http://localhost:3000/taker/#/info');
   });
+
+  it('should contain request feature and report bug buttons', function() {
+    browser.get('http://localhost:3000/taker/#/info');
+    element.all(by.css('#content span.button.shadow.teal-medium')).then(function(items) {
+      expect(items.length).toBe(2);
+      expect(items[0].getText()).toBe('request feature');
+      expect(items[1].getText()).toBe('report bug');
+    });
+  });
+
+
+
+
 });
