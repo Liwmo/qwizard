6c28be52f71d63478a2ca25fafc0d53c33860761
username <station3@qwizard.com>
2015-07-14 13:17:16 -0500

    15 - Fixed questionText to bind to contents of the editable div, allow quiz to be published. - MK JH

--- public/maker/directives/makerQuestion.js
+++ public/maker/directives/makerQuestion.js
@@ -17,6 +17,43 @@ app.directive("makerQuestion", function(){
 			scope.tf = function(value){
 				scope.correctAnswer[0] = value;
 			};
+<<<<<<< Updated upstream
+=======
+
+			scope.setDefaultPoints = function() {
+				if(scope.questionType == 'tf') {
+					scope.points = 2;
+				}
+				else if(scope.questionType == 'mc') {
+					scope.points = 2;
+				}
+				else {
+					scope.points = 5;
+				}
+			};
+
+			scope.mc = scope.tf;
+
+			// var textfields = document.getElementsByClassName("large"); 
+			// for(i=0; i<textfields.length; i++){
+   // 				textfields[i].addEventListener("keypress", function(e) {
+   //      			if(this.innerHTML.length >= this.getAttribute("max")){
+   //         				e.preventDefault();
+   //          			return false;
+   //      			}
+   //  			}, false);
+			// }
+
+			var textfield = document.querySelector(".question-text"); 
+   			textfield.addEventListener("keypress", function(e) {
+        		if(this.innerHTML.length >= this.getAttribute("max")){
+           			e.preventDefault();
+            		return false;
+        		}
+        		scope.questionText = this.innerHTML;
+    		}, false);
+
+>>>>>>> Stashed changes
 			//SUPER DUPER IMPORTANT TODO: scope.possibleAnswers should be created to look EXACTLY
 			//  as our quiz object expects it.  For example, a multiple choice would look like:
 			//  scope.possibleAnswers =  ["2 years","3 years","4 years","5 years"]
