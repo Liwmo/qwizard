b26f7ffe0213645be20451ea457c7fe48dcf1061
username <station3@qwizard.com>
2015-07-17 15:46:32 -0500

    No tests. No problems.. (But really trying to get protractor to work) - EI

--- public/maker/p-tests/conf.js
+++ public/maker/p-tests/conf.js
@@ -2,9 +2,6 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['*-test.js'],
   capabilities: {
-  	'browserName': 'chrome',
-  	'chromeOptions': {
-  		'args': ['show-fps-counter=true', 'incognito']
-  	}
+  	'browserName': 'firefox'
   }
 };
