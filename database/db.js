30f563309bc8e641988d78965d1e8110b3d9fb2f
intern <intern@asynchrony.com>
2015-07-22 15:37:41 -0500

    192 - Now defaults to a Dev environment instead of production - Waren-G DK

--- database/db.js
+++ database/db.js
@@ -1,23 +1,23 @@
 var mysql = require('mysql');
 console.log(process.env.NODE_ENV);
-var isDevelopment = process.env.NODE_ENV == 'development';
+var isProd = process.env.NODE_ENV == 'production';
 
 
-if (isDevelopment) {
-	console.log("ALERT: Dev environment, using qwizard_dev")
-	var pool = mysql.createPool({
-	    host: 'localhost',
-	    user: 'root',
-	    password: '',
-	    database: 'qwizard_dev'
-	});
-} else {
+if (isProd) {
 	console.log("ALERT: Production environment, using qwizard")
 	var pool = mysql.createPool({
 	    host: 'localhost',
 	    user: 'root',
 	    password: '',
 	    database: 'qwizard'
+	});
+} else {
+	console.log("ALERT: Dev environment, using qwizard_dev")
+	var pool = mysql.createPool({
+	    host: 'localhost',
+	    user: 'root',
+	    password: '',
+	    database: 'qwizard_dev'
 	});
 }
 
