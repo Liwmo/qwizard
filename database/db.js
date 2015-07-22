1ee833cfc012c79be65166b358fd301d74f06a48
intern <intern@asynchrony.com>
2015-07-22 15:22:47 -0500

    192 - Check for dev env to determine DB to use - DK Warren-G

--- database/db.js
+++ database/db.js
@@ -1,9 +1,24 @@
 var mysql = require('mysql');
-var pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'qwizard'
-});
+console.log(process.env.NODE_ENV);
+var isDevelopment = process.env.NODE_ENV == 'development';
+
+
+if (isDevelopment) {
+	console.log("ALERT: Dev environment, using qwizard_dev")
+	var pool = mysql.createPool({
+	    host: 'localhost',
+	    user: 'root',
+	    password: '',
+	    database: 'qwizard_dev'
+	});
+} else {
+	console.log("ALERT: Production environment, using qwizard")
+	var pool = mysql.createPool({
+	    host: 'localhost',
+	    user: 'root',
+	    password: '',
+	    database: 'qwizard'
+	});
+}
 
 module.exports = pool;
