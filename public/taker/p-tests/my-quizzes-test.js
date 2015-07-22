d79bd29f274a5890564a7cf15cdaa7265039de26
username <station3@qwizard.com>
2015-07-22 08:25:58 -0500

    199 - my quizzes tests passing, email-token-test ignoreSynchronization set false, waiting for SVG - NS

--- public/taker/p-tests/my-quizzes-test.js
+++ public/taker/p-tests/my-quizzes-test.js
@@ -28,7 +28,7 @@ describe('My Quizzes: ', function() {
 	    };
         browser.addMockModule('httpBackendMock', httpBackendMock);
 	    browser.get('http://localhost:3000/taker/#/myQuizzes');
-  		browser.sleep(2000);
+	    browser.sleep(500);
   		expect(element(by.id('liveQuizzes')).getAttribute('class')).toMatch('ng-hide');
 		browser.removeMockModule('httpBackendMock');
   	});
@@ -49,17 +49,39 @@ describe('My Quizzes: ', function() {
 
 		browser.addMockModule('httpBackendMock', httpBackendMock);
 		browser.get('http://localhost:3000/taker/#/myQuizzes');
-		browser.sleep(2000);
+		browser.sleep(500);
+
 		expect(element(by.id('liveQuizzes')).getAttribute('class')).toNotMatch('ng-hide');
 		browser.removeMockModule('httpBackendMock');
 	});
 
-	// it("should begin quiz by clicking on a row in live quizzes", function(){
+	it("should begin quiz by clicking on a row in live quizzes", function(){
+		var httpBackendMock = function() {
+	        angular.module('httpBackendMock', ['ngMockE2E', 'app'])
+	          .run(function($httpBackend) {
 
-	// });
+	            $httpBackend.whenGET('/api/quiz/').respond(function(method, url, data, headers) {
+	                return [200, [{title: 'Mock Quiz', results: '2020-03-14T05:00:00.000Z', id: 123456789}], {}];
+	            });
+
+	            $httpBackend.whenGET('/api/quiz/123456789').respond(function(method, url, data, headers) {
+	                return [200, {id: 123456789, title: "Mock Title", questions: []}, {}];
+	            });
+
+	            $httpBackend.whenGET(/.*/).passThrough();
+	            $httpBackend.whenPOST(/.*/).passThrough();
+	         });
+	    };
+
+		browser.addMockModule('httpBackendMock', httpBackendMock);
+		browser.get('http://localhost:3000/taker/#/myQuizzes');
+		browser.sleep(500);
+		element(by.css('.quizName')).click();
+		expect(browser.getCurrentUrl()).toMatch('/quiz/123456789');
+		browser.removeMockModule('httpBackendMock');
+	});
 
 	it('logout', function() {
-        browser.removeMockModule('httpBackendMock');
         browser.get('http://localhost:3000/logout');
     });
 
