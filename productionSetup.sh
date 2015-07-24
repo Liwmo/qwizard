d78d16c5b75c1a98a41bc3cb825c84be0f9b17f9
intern <intern@asynchrony.com>
2015-07-24 10:19:05 -0500

    192 - removed setcap command from productionSetup.sh per instruction from IT - MF

--- productionSetup.sh
+++ productionSetup.sh
@@ -4,7 +4,6 @@ npm install forever -g
 
 #Allows node to run on ports less than 1024
 NODE_PATH=$(which node)
-setcap 'cap_net_bind_service=+ep' $NODE_PATH
 
 sudo yum install mysql
 sudo yum install mysql-server
