421e2023b07d198669db4cef56d057aa2016a14c
intern <intern@asynchrony.com>
2015-07-24 13:25:08 -0500

    224 - added mysql script to make mysql user, added it to productionSetup - MK MF

--- productionSetup.sh
+++ productionSetup.sh
@@ -9,6 +9,7 @@ sudo yum install mysql
 sudo yum install mysql-server
 sudo cp database/my.cnf /etc/my.cnf
 sudo service mysqld start
+sudo mysql -u root < database/mysqlUser.mysql
 
 npm install --production
 npm run-script init-db
