8c62e248f3e9f778748afffc4fb01458104ee674
intern <intern@asynchrony.com>
2015-07-27 13:51:05 -0500

    232 - Favicon added - NS JH

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 app.use(logger(':date  User IP: :remote-addr ":method :url HTTP/:http-version" - :status'));
 app.use(bodyParser.json());
