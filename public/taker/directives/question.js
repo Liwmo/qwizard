66bfb2095051e8a7e0f55494de7467020b0149a8
intern <intern@intern.org>
2015-07-28 11:29:13 -0500

    63 - Styling, Drop willll append targe to drop container - DK EI

--- public/taker/directives/question.js
+++ public/taker/directives/question.js
@@ -1,4 +1,4 @@
-app.directive("question", function(){
+app.directive("question", ['ngDraggable', function(){
 	return {
 		restrict: 'E',
 		scope: {
@@ -30,6 +30,28 @@ app.directive("question", function(){
 			scope.getType = function(){
 				return '/taker/directives/templates/' + scope.type + '.html';
 			}
+
+			scope.clues = [];
+			scope.options = [];
+			if (scope.type == 'ma') {
+				for (var i = 0; i < scope.answers.length; i++) {
+					var pair = scope.answers[i].split(':');
+					scope.clues[i] = pair[0];
+					scope.options[i] = pair[1];
+				}
+			}
+
+			// scope.onDragComplete = function(data, e) {
+			// 	console.log(data);
+			// 	console.log("Successful Drag: ", e);
+			// }
+
+			scope.onDropComplete = function(dropIndex, data, e) {
+				console.log("Successful Drop:", data.index);
+				console.log(e);
+				e.element[0].classList.add("dropped");
+				document.querySelector("#drop" + dropIndex).appendChild(e.element[0]);
+			}
 		}
 	};
-});
+}]);
