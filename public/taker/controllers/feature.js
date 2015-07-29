8fc2508f427e856c529e21e923c40794b29b5b79
username <station3@qwizard.com>
2015-07-29 13:47:01 -0500

    218 - can send email. - EI JH

--- public/taker/controllers/feature.js
+++ public/taker/controllers/feature.js
@@ -1,3 +1,7 @@
 app.controller('feature', ['$scope', function($scope) {
-    
+    $scope.request = function(){
+    	var email = 'qwizard@asyncrhony.com';
+    	var subject = 'Request a Feature';
+    	window.open('mailto:' + email + '?subject=' + subject);
+    };
 }]);
