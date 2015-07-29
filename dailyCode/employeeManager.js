a640f89c7a35f1f403c4385d8b14fb863f89b6ff
intern <intern@asynchrony.com>
2015-07-29 17:48:24 -0500

    245 WIP - Working on function to save images with userid instead of name - MF WL

--- dailyCode/employeeManager.js
+++ dailyCode/employeeManager.js
@@ -1,13 +1,18 @@
 var employeeRepository = require('./employeeRepository');
 var imageWriter = require('./imageWriter');
+var db = require('../database/db');
 var isProduction = process.env.NODE_ENV == 'production';
 
 function grabEmployees() {
 	employeeRepository.getAllEmployees(function(data){
-		data.forEach(function(employee) {
+		data.forEach(function(employee, index) {
 			if(typeof employee.thumbnailPhoto !== 'object') {
 				return;
 			}
+
+			//console.log(employee.cn + '');
+			// console.log(pathToFile(employee.cn).toString('utf-8'));
+			// console.log(typeof employee.cn);
 
 			// Filter out images that are less than 1KB or greater than 10KB, as these
 			// images aren't viewable.
@@ -16,15 +21,51 @@ function grabEmployees() {
 				return;
 			}
 
+			var cn = employee.cn + '';
+			// function call to get employeeId, if it returns -1 then dont save image
+			
+			if(index == 100) {
+				var id = 'something';
+				id = insertEmployee('mike.johnson');
+				console.log(id)
+			}
+
+			
+			
+
 			imageWriter.writeImage(pathToFile(employee.cn), employee.thumbnailPhoto);
 		});
 	});
 }
 
-function pathToFile(name) {
+function pathToFile(filename) {
 	return (isProduction ? '/opt/qwizard/public/images/employees/' : 
 		'../public/images/employees/') +
-		 name + '.jpg';
+		 filename + '.jpg';
+}
+
+function getEmployeeId(name, callback) {
+	db.query('SELECT id FROM users WHERE name=?', name, function(err, message) {
+		if(err) {
+			return -1;
+		}
+
+		if(message.length) {
+			return message[0].id;
+		}
+
+		return insertEmployee(name);
+	});
+}
+
+function insertEmployee(name) {
+	db.query('INSERT INTO users SET name=?', name, function(err, message) {
+		if(err) {
+			return -1;
+		}
+
+		return message.insertId;
+	});
 }
 
 module.exports = {
