dfd245fa5c15b6dad622998ab2791326980214d1
username <station3@qwizard.com>
2015-08-03 13:18:47 -0500

    155 - Added submitted column to results in db, changed quiz API to set it to 1 on submit - NS

--- routes/api/maker/manage.js
+++ routes/api/maker/manage.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 var db = require('../../../database/db.js');
 
-router.get('/finished', function(req, res){
-	res.send("ok");
+router.get('/finished', function(req, res) {
+	var query =  'SELECT q.publish, q.results, q.title, q.id ';
+		query += 'FROM quizzes q ';
+		query += 'WHERE q.results<=? ';
+		query += 'ORDER BY q.results DESC';
+
+	var today = (new Date()).toISOString().substr(0,10);
+
+	db.query(query, [today], function(err, message) {
+		if(err) {
+			res.send({error: 'Finished quiz query failed'});
+		}else{
+			res.send(message);
+		}
+	});
 });
 
 module.exports = router;
