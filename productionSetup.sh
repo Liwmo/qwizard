4c7511e716186205d3c900afdd10441ac1bdb687
intern <intern@asynchrony.com>
2015-08-04 15:46:22 -0500

    Working with IT, changes for production - DK

--- productionSetup.sh
+++ productionSetup.sh
@@ -1,15 +1,19 @@
-sudo yum install nodejs
-sudo yum install npm
-npm install forever -g
+#!/bin/bash
+#
+#
+
+#sudo yum install nodejs
+#sudo yum install npm
+#npm install forever -g
 
 NODE_PATH=$(which node)
 
-sudo yum install mysql
-sudo yum install mysql-server
-sudo cp database/my.cnf /etc/my.cnf
-sudo service mysqld start
+#sudo yum install mysql
+#sudo yum install mysql-server
+#sudo cp database/my.cnf /etc/my.cnf
+#sudo service mysqld start
 
 npm install --production
 npm run-script init-db
 
-sudo crontab productionCrontab.txt
+crontab productionCrontab.txt
