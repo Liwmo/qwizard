f506aae75d2e89930c04a1e806ef9775dbe7d2e9
intern <intern@asynchrony.com>
2015-08-04 13:41:11 -0500

    234 - about to pull master, no changes have been made - EI

--- public/maker/controllers/finished-quiz.js
+++ public/maker/controllers/finished-quiz.js
@@ -1,10 +1,33 @@
-app.controller('finished-quiz', 'quizFactory', 'resultsFactory', '$routeParams', ['$scope', function($scope, quizFactory, resultsFactory, $routeParams) {
+app.controller('finished-quiz', ['$scope', 'quizFactory', '$routeParams',  function($scope, quizFactory, $routeParams) {
 
-    // $scope.average;
-    // $scope.max;
-    // $scope.dates;
+    /*   */$scope.average;
+    /* X */$scope.max;
+    /* X */$scope.quizId = $routeParams.id;
+    /* X */$scope.quiz;
+    /* X */$scope.closeDate;
+    /*   */$scope.openDate;
+    /* X */$scope.numQuestions;
+    /* X */$scope.quizTitle;
+    /*   */$scope.totalEmployees;
+    /*   */$scope.activeEmployees;
+    /*   */$scope.participation;
+    // $scope.participation = $scope.activeEmployees/($scope.totalEmployees || 1);
     
-    // console.log($routeParams.id);
-    // var quiz = quizFactory.getLiveQuiz($routeParams.id);
+    quizFactory.getLiveQuiz($scope.quizId, function(data) {
+    	$scope.quiz = data;
+    	$scope.quizTitle = $scope.quiz.title;
+    	console.log(data);
+    	$scope.closeDate = $scope.quiz.closeDate
+    									.substr(0, 10)
+    									.replace(/-/g, '/');
+    	$scope.numQuestions = $scope.quiz.questions.length;
+    });
+
+    quizFactory.getQuizResults($scope.quizId, function(data) {
+    	$scope.max = JSON.parse(data.pointvalues)
+    							.reduce(function(a, b) {
+    								return a + b;
+    							}, 0);
+    });
 
 }]);
