c95814ba9efd4adda177c8dcff06d5810a3480a2
intern <intern@asynchrony.com>
2015-08-04 10:46:27 -0500

    234 - basic stats styling, resolving factory merger conflics - DK EI

--- public/maker/controllers/finished-quiz.js
+++ public/maker/controllers/finished-quiz.js
@@ -1,9 +1,10 @@
 app.controller('finished-quiz', 'quizFactory', 'resultsFactory', '$routeParams', ['$scope', function($scope, quizFactory, resultsFactory, $routeParams) {
 
-    $scope.average;
-    $scope.max;
-    $scope.dates;
+    // $scope.average;
+    // $scope.max;
+    // $scope.dates;
     
-    var quiz = quizFactory.getLiveQuiz($routeParams.id);
+    // console.log($routeParams.id);
+    // var quiz = quizFactory.getLiveQuiz($routeParams.id);
 
 }]);
