241f5a00af6bc893a17a10a8a39496731e51dd53
intern <intern@intern.org>
2015-08-05 13:28:29 -0500

    273 - added new file productionUpdate - MF DK

--- productionSetup.sh
+++ productionSetup.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-#
-#
 
-#sudo yum install nodejs
-#sudo yum install npm
-#npm install forever -g
+
+sudo yum install nodejs
+sudo yum install npm
+npm install forever -g
 
 NODE_PATH=$(which node)
 
-#sudo yum install mysql
-#sudo yum install mysql-server
-#sudo cp database/my.cnf /etc/my.cnf
-#sudo service mysqld start
+sudo yum install mysql
+sudo yum install mysql-server
+sudo cp database/my.cnf /etc/my.cnf
+sudo service mysqld start
 
 npm install --production
-#mysql -u qwizard -p'iS#0&dZoT@&$Vi' --socket /var/run/mysql-qwizard/mysqld.sock < database/createEmptyDatabase.mysql
+mysql -u qwizard -p'iS#0&dZoT@&$Vi' --socket /var/run/mysql-qwizard/mysqld.sock < database/createEmptyDatabase.mysql
 
 crontab productionCrontab.txt
